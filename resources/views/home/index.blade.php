63f8321745c38927df0cbd1a1ea287a27dc26437
bahri444 <bahrysaipul266@gmail.com>
2023-01-12 00:36:38 +0800

    update index & header

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,21 +1,31 @@
 @include('layout.header')
-<div class="container">
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            @foreach($kegiatan as $keg)
-            <div class="carousel-item active">
-                <img src="foto_kegiatan/{{$keg->foto_keg}}" class="d-block w-100" alt="404">
+<div class="learning_part">
+    <div class="container">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            @endforeach
+            <div class="carousel-inner">
+                @foreach($kegiatan as $keg)
+                <div class="carousel-item active">
+                    <img src="foto_kegiatan/{{$keg->foto_keg}}" class="d-block w-100" alt="404">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>{{$keg->nama_kegiatan}}</h5>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
 </div>
 
@@ -26,7 +36,7 @@
         <div class="row align-items-sm-center align-items-lg-stretch mt-3">
             <div class="col-md-5 col-lg-5">
                 <div class="learning_img">
-                    <img src="foto_kegiatan/{{$act->foto_keg}}" width="450px" height="350px" alt="404">
+                    <img src="foto_kegiatan/{{$act->foto_keg}}" class="d-block w-100" alt="404">
                 </div>
             </div>
             <div class="col-md-7 col-lg-7">
